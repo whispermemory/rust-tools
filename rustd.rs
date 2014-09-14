d6a9b242520dacb1d5aca93707363d515b3bd7a0
lk <whispermemory@gmail.com>
2014-09-14 20:54:49 +0800

    rust version of rust local doc

--- rustd.rs
+++ rustd.rs
@@ -1,7 +1,13 @@
 extern crate collections;
+extern crate libc;
+extern crate getopts;
 
 use collections::string::String ;
 use std::vec::Vec;
+use getopts::{optflag,getopts,optopt};
+use std::os;
+use libc::funcs::c95::stdlib::exit;
+use std::io::TcpStream;
 
 #[allow(dead_code)]
 struct Doc {
@@ -12,12 +18,42 @@ struct Doc {
 
 
 // download doc if not exists
+
+fn usage() ->String {
+    println!("rustd -l : show crate list");
+    println!("rustd cratename : show webdoc named cratename in terminal");
+    return String::new();
+}
+
 fn main(){
     //string is size-fixed
     //    #[allow(dead_code)]
-    print_crate_list();
+    let args: Vec<String> = os::args();
 
+    let opts = [
+        optflag("l", "list", "list crates"),
+        optflag("h", "help", "print help"),
+        optopt("d", "","download", "download doc"),
+        ];
+    
+    let matches = match getopts(args.tail(), opts) {
+        Ok(m) => {m},
+        Err(f) => {fail!(f.to_string())}
+    };
 
+    let optstr =  matches.opt_str("l");
+    match optstr {
+        Some(_)=> {},
+        None => print_crate_list(),
+    };
+
+    let downDoc = matches.opt_str("d");
+    match downDoc {
+        Some(x) => wget_crate(x),
+        _ => {usage()},
+    };
+
+    wget_crate(String::from_str("std"));
 }
 
 #[allow(dead_code)]
@@ -29,60 +65,31 @@ fn crate_name() ->   Vec<&'static str>  {
 
 }
 
+#[allow(dead_code)]
 fn print_crate_list() {
     let cl = crate_name();
     for c in cl.iter() {
         println!("{}",c);
     }
+    //    unsafe{exit(0);}
 }
 
 
-#[allow(dead_code)]
-fn down_crate_module_list(name : String) -> Vec<String> {
-    //    let url =  doc_path();
-    wget_html(url)
-}
-
-
-
-//crate
-//module 
-//
-fn down_doc(base:String, v:String, c:String, m:String){
-    let doc_path = down_doc(base,c,m,v);
-
-
-}
-
-fn doc_path(base : String, c : String, m : String, version : String) -> String {
-    let path = String::new();
-
-    if base.len() <= 0 {
-        return String::new();
-    }
+fn wget_crate(name : String) -> String  {
+    let baseurl = "54.215.0.2";
+    let  mut s = TcpStream::connect(baseurl,80);
+    let mut stream = match s {
+        Ok(t) => t,
+        Err(f) => {fail!(f.desc)},
+    };
     
-    path.append("/").append(base);
-    if version.len > 0 {
-        version.append("/");
-        path.append(version);
-    }
-    if c.len > 0 {
-        c.append("/");
-        if m.len > 0 {
-            m.append("/")
-        }
-        c.append(m);
-    }
-    
-    return path.append(c);
-}
-
-fn wget_html(url : String) -> String {
-    
-}
-
-fn poll_modules(htmlTag : String, html : String) {
-    
+    let req_data = "GET / HTTP/1.1\r\nHost: doc.rust-lang.org\r\nConnection: Close\r\nUser-Agent: Wget/1.1.5\r\n\r\n";
+    stream.write_str(req_data);
+    let back = stream.read_to_string();
+    stream.close_read();
+    let doccontent = back.unwrap();
+    drop(stream);
+    return doccontent;
 }
 
 
